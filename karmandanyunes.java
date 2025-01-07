22843bb4492da1399e6d721ba93d360ef16c3a10
yunes.negarestani <97680335+yunesnegarestani@users.noreply.github.com>
2025-01-07 00:27:32 -0800

    Add files via upload

---
+++ karmandanyunes.java
@@ -0,0 +1,66 @@
+import java.util.*;
+
+public class karmandanyunes {
+    public static void main(String[] args) 
+  {
+    Scanner inscanner=new Scanner(System.in);
+    String[]name=new String[3];
+    String[]semat=new String[3];
+    Double[]date=new Double[3];
+    
+    System.out.println("Enter your details");
+    for (int i=0;i<3 ;i++) 
+  {
+    System.out.println("ENTER NAME");
+    name[i]=inscanner.nextLine();
+    System.out.println(" ENTER SEMAT");
+    semat[i]=inscanner.nextLine();
+    System.out.println(" ENTER VORODY ");
+    date[i]=inscanner.nextDouble();
+    inscanner.nextLine();
+  }
+  
+    for (int i=0;i<3 ;i++ ) 
+    {
+        
+        
+       if (name[i].equals("negarestani"))  
+       {
+           System.out.println(" negarestani details ");
+           System.out.println( " name:  "+name[i]+ "semat:  "+semat[i]+" vorody:  "+date[i]);
+       }
+       
+       if (date[i]== 1384)
+    {
+        System.out.println("vorody 1384");
+         System.out.println( " name:  "+name[i]+ "semat:  "+semat[i]+" vorody:  "+date[i]);
+        
+    }
+     if (semat[i].equals("moaven"))  
+       {
+           System.out.println("moaven details ");
+           System.out.println( " name:  "+name[i]+ "semat:  "+semat[i]+" vorody:  "+date[i]);
+       }
+    
+     if (semat[i].equals("modir"))  
+       {
+           System.out.println("modir details ");
+           System.out.println( " name:  "+name[i]+ "semat:  "+semat[i]+" vorody:  "+date[i]);
+       }
+    
+    
+    
+    
+    }
+    
+
+    
+    
+    
+    
+    
+    
+      
+    
+  }
+}
